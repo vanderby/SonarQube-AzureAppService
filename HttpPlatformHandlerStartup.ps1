b2a67b072deed33b05fa338b0e30440c4a516bd1
Nathan Vanderby <28767221+vanderby@users.noreply.github.com>
2018-11-17 15:24:15 -0600

    Initial Commit

---
+++ HttpPlatformHandlerStartup.ps1
@@ -0,0 +1,32 @@
+﻿function log($message) {
+    [DateTime]$dateTime = [System.DateTime]::Now
+    Write-Host "$($dateTime.ToLongTimeString()) $message" 
+}
+
+log('Starting HttpPlatformHandler Script')
+
+$port = $env:HTTP_PLATFORM_PORT
+log("HTTP_PLATFORM_PORT is: $port")
+
+log("Searching for sonar.properties file")
+$propFile = Get-ChildItem 'sonar.properties' -Recurse
+if(!$propFile) {
+    log("Could not find sonar.properties")
+    exit
+}
+
+log("File found at: $($propFile.FullName)")
+log("Writing to sonar.properties file")
+$configContents = Get-Content -Path $propFile.FullName -Raw
+$configContents -ireplace '#?sonar.web.port=.+', "sonar.web.port=$port" | Set-Content -Path $propFile.FullName
+
+log("Searching for StartSonar.bat")
+$startScript = Get-ChildItem 'StartSonar.bat' -Recurse
+if(!$startScript) {
+    log("Could not find StartSonar.bat")
+    exit
+}
+
+log("File found at: $($startScript[-1].FullName)")
+log("Executing StartSonar.bat")
+& $startScript[-1].FullName
---
+++ Upload-SonarQubeToAzure.ps1
@@ -0,0 +1,47 @@
+﻿Write-Host 'Querying For Web Apps'
+Login-AzureRmAccount
+$webApps = Get-AzureRmWebApp
+if(!$webApps) {
+    Write-Host 'No web apps found.'
+    exit
+}
+
+for($i=0; $i -lt $webApps.Count; $i++) {
+    Write-Host "[$i] $($webApps[$i].Name)"
+}
+
+$input = Read-Host 'Select the web app to upload SonarQube to'
+if ($input -lt 0 -or $input -gt $webApps.Count) {
+    Write-Host 'Value is outside of expected range.'
+    exit
+}
+
+$webApp = $webApps[$input]
+Write-Host "  WebApp: '$($webApp.Name)' Resource Group: '$($webApp.ResourceGroup)'"
+
+Write-Host 'Setting Security to TLS 1.2'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Write-Host 'Getting a list of downloads'
+$downloadSource = 'https://binaries.sonarsource.com/Distribution/sonarqube/'
+$allDownloads = Invoke-WebRequest -Uri $downloadSource
+$zipFiles = $allDownloads.Links | Where-Object { $_.innerText.EndsWith('.zip') -and !($_.innerText.contains('alpha') -or $_.innerText.contains('RC')) }
+$latestFile = $zipFiles[-1]
+$downloadUri = $downloadSource + $latestFile.href
+
+Write-Host "Downloading '$downloadUri'"
+$outFile = '.\SonarQube.zip'
+Invoke-WebRequest -Uri $downloadUri -OutFile $outFile
+
+Write-Host 'Getting FTP Upload User Info'
+$publishSettings = [xml](Get-AzureRmWebAppPublishingProfile -Name $webApp.Name -ResourceGroupName $webApp.ResourceGroup -OutputFile null)
+$website = $publishSettings.SelectSingleNode("//publishData/publishProfile[@publishMethod='MSDeploy']")
+$username = $webSite.userName
+$password = $webSite.userPWD
+
+Write-Host 'Uploading Zip Deployment'
+$apiUrl = "https://sonarqubeappserviceydhwkgdf7xkqc.scm.azurewebsites.net/api/zipdeploy"
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
+$userAgent = "powershell/1.0"
+Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method POST -InFile $outFile -ContentType "multipart/form-data" -Verbose
+Write-Host 'Upload Complete.'
