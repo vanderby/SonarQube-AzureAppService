4772cbea892184e7c6a8c2a932c23f75f00219b1
vanderby <28767221+vanderby@users.noreply.github.com>
2018-11-30 15:54:15 -0600

    Single deployment (#5)
    * Adding post deployment script. Adding download script. Removing upload script.
    
    * Fixing download script for Azure deployment
    
    * Update deployment script to copy runtime files. Moved runtime files for easy copying. Renamed files accordingly.
    
    * Correcting zip download and extraction folder
    
    * Changing Write-Host to Write-Output tofix runtime error
    
    * Adding logic to replace default java wrapper java command. Increasing startup timeout to 5 minutes.
    
    * Updating readme.md

---
+++ Deploy-SonarQubeAzureAppService.ps1
@@ -0,0 +1,23 @@
+﻿Write-Output 'Copy wwwroot folder'
+xcopy wwwroot ..\wwwroot /Y
+
+Write-Output 'Setting Security to TLS 1.2'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Write-Output 'Prevent the progress meter from trying to access the console'
+$global:progressPreference = 'SilentlyContinue'
+
+Write-Output 'Getting a list of downloads'
+$downloadSource = 'https://binaries.sonarsource.com/Distribution/sonarqube/'
+$allDownloads = Invoke-WebRequest -Uri $downloadSource -UseBasicParsing
+$zipFiles = $allDownloads[0].Links | Where-Object { $_.href.EndsWith('.zip') -and !($_.href.contains('alpha') -or $_.href.contains('RC')) }
+$latestFile = $zipFiles[-1]
+$downloadUri = $downloadSource + $latestFile.href
+
+Write-Output "Downloading '$downloadUri'"
+$outputFile = "..\wwwroot\$($latestFile.href)"
+Invoke-WebRequest -Uri $downloadUri -OutFile $outputFile -UseBasicParsing
+Write-Output 'Done downloading file'
+
+Write-Output 'Extracting zip'
+Expand-Archive -Path $outputFile -DestinationPath ..\wwwroot
+Write-Output 'Extraction complete'
