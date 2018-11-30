4c06a9fc41bada16b737ff52e6545c7f8ac3c6cf
Nathan Vanderby <28767221+vanderby@users.noreply.github.com>
2018-11-30 14:02:54 -0600

    Fixing download script for Azure deployment

--- Download-SonarQube.ps1
+++ Download-SonarQube.ps1
@@ -1,7 +1,7 @@
 ﻿Write-Output 'Setting Security to TLS 1.2'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Write-Output 'Prevent the progress meter from trying to access the console mode'
-$ProgressPreference = "SilentlyContinue"
+Write-Output 'Prevent the progress meter from trying to access the console'
+$global:progressPreference = 'SilentlyContinue'
 
 Write-Output 'Getting a list of downloads'
 $downloadSource = 'https://binaries.sonarsource.com/Distribution/sonarqube/'
@@ -15,5 +15,5 @@ Invoke-WebRequest -Uri $downloadUri -OutFile $latestFile.href -UseBasicParsing
 Write-Output 'Done downloading file'
 
 Write-Output 'Extracting zip'
-Expand-Archive $latestFile.href -DestinationPath .
+Expand-Archive -Path $latestFile.href -DestinationPath .
 Write-Output 'Extraction complete'
