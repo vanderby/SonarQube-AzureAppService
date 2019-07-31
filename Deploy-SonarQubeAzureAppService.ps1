7619d433d1db258670a44ac6086bc94e745c7b26
Nathan Vanderby <28767221+vanderby@users.noreply.github.com>
2019-07-30 19:09:50 -0500

    Add a sort method with a custom expresison that calculates the version from the file name

--- Deploy-SonarQubeAzureAppService.ps1
+++ Deploy-SonarQubeAzureAppService.ps1
@@ -10,7 +10,11 @@ Write-Output 'Getting a list of downloads'
 $downloadSource = 'https://binaries.sonarsource.com/Distribution/sonarqube/'
 $allDownloads = Invoke-WebRequest -Uri $downloadSource -UseBasicParsing
 $zipFiles = $allDownloads[0].Links | Where-Object { $_.href.EndsWith('.zip') -and !($_.href.contains('alpha') -or $_.href.contains('RC')) }
-$latestFile = $zipFiles[-1]
+
+# We sort by a custom expression so that we sort based on a version and not as a string. This results in the proper order given values such as 7.9.zip and 7.9.1.zip.
+#   In the expression we use RegEx to find the "Version.zip" string, then split and grab the first to get just the "Version" and finally cast that to a version object
+$sortedZipFiles = $zipFiles | Sort-Object -Property @{ Expression = { [Version]([RegEx]::Match($_.href, '\d+.\d+.?(\d+)?.zip').Value -Split ".zip")[0] } }
+$latestFile = $sortedZipFiles[-1]
 $downloadUri = $downloadSource + $latestFile.href
 
 Write-Output "Downloading '$downloadUri'"
