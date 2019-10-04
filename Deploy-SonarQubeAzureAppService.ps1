db730d85ade42ff81323b23bdd1cd75b11ddf757
vanderby <28767221+vanderby@users.noreply.github.com>
2019-10-03 22:38:31 -0500

    Add Telemetry (#14)
    Adding telemetry to scripts.

--- Deploy-SonarQubeAzureAppService.ps1
+++ Deploy-SonarQubeAzureAppService.ps1
@@ -1,27 +1,87 @@
-﻿Write-Output 'Copy wwwroot folder'
-xcopy wwwroot ..\wwwroot /Y
+﻿param(
+    [string]$ApplicationInsightsApiKey = $Env:Deployment_Telemetry_Instrumentation_Key
+)
 
-Write-Output 'Setting Security to TLS 1.2'
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Write-Output 'Prevent the progress meter from trying to access the console'
-$global:progressPreference = 'SilentlyContinue'
+function TrackTimedEvent {
+    param (
+        [string]$InstrumentationKey,
+        [string]$EventName,
+        [scriptblock]$ScriptBlock,
+        [Object[]]$ScriptBlockArguments
+    )
 
-Write-Output 'Getting a list of downloads'
-$downloadSource = 'https://binaries.sonarsource.com/Distribution/sonarqube/'
-$allDownloads = Invoke-WebRequest -Uri $downloadSource -UseBasicParsing
-$zipFiles = $allDownloads[0].Links | Where-Object { $_.href.EndsWith('.zip') -and !($_.href.contains('alpha') -or $_.href.contains('RC')) }
+    [System.Diagnostics.Stopwatch]$stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+    Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $ScriptBlockArguments
+    $stopwatch.Stop()
 
-# We sort by a custom expression so that we sort based on a version and not as a string. This results in the proper order given values such as 7.9.zip and 7.9.1.zip.
-#   In the expression we use RegEx to find the "Version.zip" string, then split and grab the first to get just the "Version" and finally cast that to a version object
-$sortedZipFiles = $zipFiles | Sort-Object -Property @{ Expression = { [Version]([RegEx]::Match($_.href, '\d+.\d+.?(\d+)?.zip').Value -Split ".zip")[0] } }
-$latestFile = $sortedZipFiles[-1]
-$downloadUri = $downloadSource + $latestFile.href
+    if($InstrumentationKey)
+    {
+        $uniqueId = ''
+        if($Env:WEBSITE_INSTANCE_ID)
+        {
+            $uniqueId = $Env:WEBSITE_INSTANCE_ID.substring(5,15)
+        }
 
-Write-Output "Downloading '$downloadUri'"
-$outputFile = "..\wwwroot\$($latestFile.href)"
-Invoke-WebRequest -Uri $downloadUri -OutFile $outputFile -UseBasicParsing
-Write-Output 'Done downloading file'
+        $properties = @{
+            "Location" = $Env:REGION_NAME;
+            "SKU" = $Env:WEBSITE_SKU;
+            "Processor Count" = $Env:NUMBER_OF_PROCESSORS;
+            "Always On" = $Env:WEBSITE_SCM_ALWAYS_ON_ENABLED;
+            "UID" = $uniqueId
+        }
 
-Write-Output 'Extracting zip'
-Expand-Archive -Path $outputFile -DestinationPath ..\wwwroot
-Write-Output 'Extraction complete'
+        $measurements = @{
+            'duration (ms)' = $stopwatch.ElapsedMilliseconds
+        }
+
+        $body = ConvertTo-Json -Depth 5 -InputObject @{
+			name = "Microsoft.ApplicationInsights.Dev.$InstrumentationKey.Event";
+			time = [Datetime]::UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
+			iKey = $InstrumentationKey;
+			data = @{
+				baseType = "EventData";
+				baseData = @{
+					ver = 2;
+					name = $EventName;
+                    properties = $properties;
+                    measurements = $measurements;
+				}
+			};
+        }
+
+        Invoke-RestMethod -Method POST -Uri "https://dc.services.visualstudio.com/v2/track" -ContentType "application/json" -Body $body | out-null
+    }
+}
+
+TrackTimedEvent -InstrumentationKey $ApplicationInsightsApiKey -EventName 'Download And Extract Binaries' -ScriptBlock {
+    Write-Output 'Copy wwwroot folder'
+    xcopy wwwroot ..\wwwroot /YI
+
+    Write-Output 'Setting Security to TLS 1.2'
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Write-Output 'Prevent the progress meter from trying to access the console'
+    $global:progressPreference = 'SilentlyContinue'
+    
+    Write-Output 'Getting a list of downloads'
+    $downloadSource = 'https://binaries.sonarsource.com/Distribution/sonarqube/'
+    $allDownloads = Invoke-WebRequest -Uri $downloadSource -UseBasicParsing
+    $zipFiles = $allDownloads[0].Links | Where-Object { $_.href.EndsWith('.zip') -and !($_.href.contains('alpha') -or $_.href.contains('RC')) }
+
+    # We sort by a custom expression so that we sort based on a version and not as a string. This results in the proper order given values such as 7.9.zip and 7.9.1.zip.
+    #   In the expression we use RegEx to find the "Version.zip" string, then split and grab the first to get just the "Version" and finally cast that to a version object
+    $sortedZipFiles = $zipFiles | Sort-Object -Property @{ Expression = { [Version]([RegEx]::Match($_.href, '\d+.\d+.?(\d+)?.zip').Value -Split ".zip")[0] } }
+    $latestFile = $sortedZipFiles[-1]
+    $downloadUri = $downloadSource + $latestFile.href
+
+    Write-Output "Downloading '$downloadUri'"
+    $outputFile = "..\wwwroot\$($latestFile.href)"
+    Invoke-WebRequest -Uri $downloadUri -OutFile $outputFile -UseBasicParsing
+    Write-Output 'Done downloading file'
+
+    TrackTimedEvent -InstrumentationKey $ApplicationInsightsApiKey -EventName 'Extract Binaries' -ScriptBlockArguments $outputFile -ScriptBlock {
+        param([string]$outputFile)
+        Write-Output 'Extracting zip'
+        Expand-Archive -Path $outputFile -DestinationPath '..\wwwroot' -Force
+        Write-Output 'Extraction complete'
+    }
+}
--- wwwroot/HttpPlatformHandlerStartup.ps1
+++ wwwroot/HttpPlatformHandlerStartup.ps1
@@ -1,9 +1,54 @@
-﻿function log($message) {
+﻿param(
+    [string]$ApplicationInsightsApiKey = $Env:Deployment_Telemetry_Instrumentation_Key
+)
+
+function log($message) {
     [DateTime]$dateTime = [System.DateTime]::Now
     Write-Output "$($dateTime.ToLongTimeString()) $message" 
 }
 
-log('Starting HttpPlatformHandler Script')
+function TrackEvent {
+    param (
+        [string]$InstrumentationKey,
+        [string]$EventName
+    )
+
+    log($EventName)
+    if($InstrumentationKey)
+    {
+        $uniqueId = ''
+        if($Env:WEBSITE_INSTANCE_ID)
+        {
+            $uniqueId = $Env:WEBSITE_INSTANCE_ID.substring(5,15)
+        }
+
+        $properties = @{
+            "Location" = $Env:REGION_NAME;
+            "SKU" = $Env:WEBSITE_SKU;
+            "Processor Count" = $Env:NUMBER_OF_PROCESSORS;
+            "Always On" = $Env:WEBSITE_SCM_ALWAYS_ON_ENABLED;
+            "UID" = $uniqueId
+        }
+
+        $body = ConvertTo-Json -Depth 5 -InputObject @{
+			name = "Microsoft.ApplicationInsights.Dev.$InstrumentationKey.Event";
+			time = [Datetime]::UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
+			iKey = $InstrumentationKey;
+			data = @{
+				baseType = "EventData";
+				baseData = @{
+					ver = 2;
+					name = $EventName;
+					properties = $properties;
+				}
+			};
+        }
+
+        Invoke-RestMethod -Method POST -Uri "https://dc.services.visualstudio.com/v2/track" -ContentType "application/json" -Body $body | out-null
+    }
+}
+
+TrackEvent -InstrumentationKey $ApplicationInsightsApiKey -EventName 'Starting HttpPlatformHandler Script'
 
 $port = $env:HTTP_PLATFORM_PORT
 log("HTTP_PLATFORM_PORT is: $port")
